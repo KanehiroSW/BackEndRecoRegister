4edd1ad1a9497918817fb80f9d4a0fcab283e27c
JoseCarden <josejesuscardenasjimenez@gmail.com>
2024-07-26 16:36:26 -0500

    Metodo POST Marcacion
    End point para guardar la hora de la marcación

--- controllers/marcaciones_controller.py
+++ controllers/marcaciones_controller.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, request,jsonify
 from models import db
+from datetime import datetime
 import cv2
 from flask_jwt_extended import JWTManager, jwt_required, get_jwt_identity,create_access_token
-from models.usuario import Usuario
+from models.marcaciones import Marcacion
 from werkzeug.security import check_password_hash,generate_password_hash
 import face_recognition
 import base64
@@ -11,3 +12,26 @@ from io import BytesIO
 from PIL import Image
 
 marcaciones_bp=Blueprint('marcaciones_bp', __name__)
+
+@marcaciones_bp.route('/nueva_marca',methods=['POST'])
+def add_marca():
+    data = request.get_json()
+    idUser = data.get('idUsuario')
+    fecha = datetime.now()
+    minTarde = data.get('min_tarde')
+    estFalta = data.get('est_falta')
+
+    # if not idUser or not fecha or not minTarde or not estFalta:
+    #     return jsonify({"error": "Faltan datos en la solicitud"}), 400
+
+    nueva_marcacion = Marcacion(
+        idUsuario = idUser,
+        fecha = fecha,
+        min_tarde = minTarde,
+        est_falta = estFalta
+    )
+    db.session.add(nueva_marcacion)
+    db.session.commit()
+
+    return jsonify({'message': 'Nueva marcación exitosa'}), 201
+
